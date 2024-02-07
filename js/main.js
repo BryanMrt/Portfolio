3456277deb099a05c07e8c07d5e4fdb4bb6b131b
bryanmrt <lbmorato@gmail.com>
2024-02-07 09:41:06 +0100

    text add and form update

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function toggleNav() {
     navigation.classList.toggle("active");
 }
 
-// activ nav
+// ACTIVE NAV
 
 const navLinkElements = document.querySelectorAll('.nav-link');
 const sectionElements = document.querySelectorAll('.section');
@@ -30,3 +30,73 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+
+// INPUT VERIFY FORM 
+
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+const notification = document.getElementById("notification");
+
+async function sendMessage(data) {
+  try {
+    // Ici, vous pouvez remplacer l'URL par celle de votre service d'envoi de formulaire
+    const response = await fetch("URL_de_votre_service", {
+      method: "POST",
+      body: data,
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+    if (response.ok) {
+      return "Thanks for your submission!";
+    } else {
+      const responseData = await response.json();
+      if (responseData.hasOwnProperty('errors')) {
+        return responseData.errors.map(error => error.message).join(", ");
+      } else {
+        return "Oops! There was a problem submitting your form";
+      }
+    }
+  } catch (error) {
+    return "Oops! There was a problem submitting your form";
+  }
+}
+
+function checkInputs() {
+  const items = document.querySelectorAll(".item");
+
+  for (const item of items) {
+    if ( item.value === "") {
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
+    }
+    item.addEventListener("keyup", () => {
+      if (item.value !== "") {
+        item.classList.remove("error");
+        item.parentElement.classList.remove("error");
+      } else {
+        item.classList.add("error");
+        item.parentElement.classList.add("error");
+      }
+    });
+  }
+}
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  checkInputs();
+
+  const data = new FormData(form);
+  const message = await sendMessage(data);
+  showNotification(message);
+});
+
+function showNotification(message) {
+  notification.textContent = message;
+  notification.style.display = "block";
+  setTimeout(() => {
+    notification.style.display = "none";
+  }, 5000); // Disparition automatique après 5 secondes
+}
